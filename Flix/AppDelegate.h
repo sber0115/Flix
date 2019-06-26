86eadef974ad5e791607d9d0b611ffbcd9cd7099
Sebastian Bernal <sbernal@andrew.cmu.edu>
2019-06-26 10:59:30 -0700

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by sbernal0115 on 6/26/19.
+//  Copyright © 2019 sbernal0115. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Flix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flix
+//
+//  Created by sbernal0115 on 6/26/19.
+//  Copyright © 2019 sbernal0115. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
